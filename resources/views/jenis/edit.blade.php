2c8fdd755bc7bbc5b3e6536ab0819bdf78f53b5f
lauramyrlla <lauramaylla@gmail.com>
2026-09-07 10:00:55 +0700

    menambahkan hint

--- resources/views/jenis/edit.blade.php
+++ resources/views/jenis/edit.blade.php
@@ -3,13 +3,15 @@
 @section('title', 'Edit Jenis')
 
 @section('content')
-<h4>Edit Jenis</h4>
+<div class="form-jenis-wrapper">
+    <h4>Edit Jenis</h4>
 
-<form action="{{ route('jenis.update', $jenis) }}"
-      method="POST"
-      enctype="multipart/form-data">
-@method('PUT')
+    <form action="{{ route('jenis.update', $jenis) }}"
+          method="POST"
+          enctype="multipart/form-data">
+        @method('PUT')
 
-@include('jenis._form')
-</form>
+        @include('jenis._form')
+    </form>
+</div>
 @endsection
